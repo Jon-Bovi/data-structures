46e44142012237e84153e58c2ef7629db438f90f
Ford Fowler <fordjfowler@gmail.com>
2016-12-12 13:18:32 -0800

    define linked list push method

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,9 +1,22 @@
+"""Module defining linked list."""
+
+
 class LinkedList(object):
+    """Classic linked list data structure."""
+
     def __init__(self, head=None):
+        """Initialize LinkedList instance."""
         self.head = head
+
+    def push(self, val):
+        """Insert val at the head of linked list."""
+        self.head = Node(val, self.head)
 
 
 class Node(object):
+    """Node class."""
+
     def __init__(self, val, next):
+        """Initialize Node instance."""
         self.val = val
         self.next = next
