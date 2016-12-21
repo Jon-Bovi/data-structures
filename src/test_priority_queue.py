e7f79c8dd5d857f2fc0f33e219dd559ecfe3fa74
Ford Fowler <fordjfowler@gmail.com>
2016-12-21 15:54:38 -0800

    testing?

--- src/test_priority_queue.py
+++ src/test_priority_queue.py
@@ -3,17 +3,45 @@ import pytest
 
 
 @pytest.fixture
-def test_pq():
+def empty_pq():
+    """Return empty initialized PriorityQueue."""
     from priority_queue import PriorityQueue
     return PriorityQueue()
 
 
-def test_pq_init(test_pq):
+@pytest.fixture
+def two_long_pq():
+    """Return PriorityQueue of length two."""
+    from priority_queue import PriorityQueue
+    pq = PriorityQueue()
+    pq.insert('value1', 3)
+    pq.insert('value2', 1)
+    return pq
+
+
+def test_pq_init(empty_pq):
     """Test priority queue init."""
-    assert len(test_pq._heap._list) == 0
+    assert len(empty_pq._heap._list) == 0
 
 
-def test_pq_insert(test_pq):
+def test_pq_insert(empty_pq):
     """Test insert into empty pqueue."""
-    test_pq.insert((1, 'val'))
-    assert len(test_pq._heap._list) == 0
+    empty_pq.insert('val', 1)
+    assert len(empty_pq._heap._list) == 1
+
+
+def test_pq_double_insert(two_long_pq):
+    """Test multiple inserts on pqueue."""
+    assert len(two_long_pq._heap._list) == 2
+
+
+def test_pop_empty(empty_pq):
+    """Test pop on empty pqueue raises IndexError."""
+    with pytest.raises(IndexError, message="Cannot pop from an empty priority queue."):
+        empty_pq.pop()
+
+
+def test_pop_non_empty(two_long_pq):
+    """Test pop returns correct item."""
+    assert two_long_pq.pop() == ('value1', 3)
+
