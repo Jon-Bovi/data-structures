db9d3769e2a9085c3ef51139ecd3680746fb34f1
Ford Fowler <fordjfowler@gmail.com>
2016-12-27 19:15:29 -0800

    init tests

--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -16,11 +16,35 @@ def another_node():
     return Node()
 
 
+def edge(node, another_node):
+    """Return an edge object."""
+    from simple_graph import Edge
+    return Edge(node, another_node)
+
+
 @pytest.fixture
 def graph():
     """Return initialized graph."""
     from simple_graph import Graph
     return Graph()
+
+
+def test_node_init(node):
+    """Test node init method does what it's supposed to."""
+    assert node.name is None
+
+
+def test_edge_init(edge):
+    """Test edge init method does what it's supposed to."""
+    from simple_graph import Node
+    assert type(edge.source) is Node
+    assert type(edge.dest) is Node
+
+
+def test_graph_init(graph):
+    """Test graph init method does what it's supposed to."""
+    assert graph.nodes == []
+    assert graph.edges == []
 
 
 def test_add_node(graph, node):
