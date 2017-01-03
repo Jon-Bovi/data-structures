53b9414fa6a23916e4b437705d433312a3c58dbb
Your Name <you@example.com>
2017-01-03 00:22:54 -0800

    added more tests. passing. need tests for depth_first_traversal_iterative and timeit

--- src/test_graph_traversal.py
+++ src/test_graph_traversal.py
@@ -33,80 +33,100 @@ def test_graph_init(graph):
 def test_add_node(graph):
     """Test add_node adds node to graph."""
     graph.add_node('node')
-    assert 'node' in graph.node_dict
+    assert 'node' in graph.nodes()
 
 
-# def test_add_edge(graph):
-#     """Test that add_edge adds edge with source and dest."""
-#     graph.add_edge('node', 'another_node')
-#     assert graph.edge_list[0].source == 'node'
-#     assert graph.edge_list[0].dest == 'another_node'
+def test_add_edge(complex_g):
+    """Test that add_edge adds edge with source and dest."""
+    assert len(complex_g.edges()) > 0
 
 
-# def test_del_node(graph):
-#     """Test del_node removes node."""
-#     graph.add_node('node')
-#     graph.del_node('node')
-#     assert 'node' not in graph.node_list
+def test_add_edge_error(complex_g):
+    """Test del_edge raises error if adds an edge that already exists."""
+    with pytest.raises(ValueError):
+        complex_g.add_edge('C', 'G')
 
 
-# def test_del_node_error(graph):
-#     """Test del_node raises error if node to be deleted doesn't exist."""
-#     graph.add_node('mama')
-#     with pytest.raises(IndexError):
-#         graph.del_node('dada')
+def test_delete_edge_error(complex_g):
+    """Test del_edge raises error if edge to be deleted doesn't exist."""
+    complex_g.add_edge('node', 'edon')
+    with pytest.raises(IndexError):
+        complex_g.del_edge('node', 'noooode')
 
 
-# def test_del_edge(graph):
-#     """Test del edge removes edge."""
-#     graph.add_edge('node', 'edon')
-#     graph.del_edge('node', 'edon')
-#     assert len(graph.edge_list) == 0
+def test_del_node(complex_g):
+    """Test del_node removes node."""
+    complex_g.add_node('node')
+    complex_g.del_node('node')
+    assert 'node' not in complex_g.node_dict
 
 
-# def test_del_edge_error(graph):
-#     """Test del_edge raises error if edge to be deleted doesn't exist."""
-#     graph.add_edge('node', 'edon')
-#     with pytest.raises(IndexError):
-#         graph.del_edge('node', 'noooode')
+def test_del_node_error(complex_g):
+    """Test del_node raises error if node to be deleted doesn't exist."""
+    complex_g.add_node('mama')
+    with pytest.raises(IndexError):
+        complex_g.del_node('dada')
 
 
-# def test_has_node_true(graph):
-#     """Test has_node returns true if node in graph."""
-#     graph.add_node('mama')
-#     assert graph.has_node('mama')
+def test_del_edge(graph):
+    """Test del edge removes edge."""
+    graph.add_edge('node', 'edon')
+    graph.del_edge('node', 'edon')
+    assert len(graph.edges()) == 0
 
 
-# def test_has_node_false(graph):
-#     """Test has_node returns false if node not in graph."""
-#     graph.add_node('mama')
-#     assert graph.has_node('dada') is False
+def test_del_edge_error(graph):
+    """Test del_edge raises error if edge to be deleted doesn't exist."""
+    graph.add_edge('node', 'edon')
+    with pytest.raises(IndexError):
+        graph.del_edge('node', 'noooode')
 
 
-# def test_neighbors(graph):
-#     """Test neighbors returns all nodes connected to a single node."""
-#     graph.add_edge('bank', 'car')
-#     graph.add_edge('wheels', 'car')
-#     assert graph.neighbors('car') == ['bank', 'wheels']
+def test_has_node_true(graph):
+    """Test has_node returns true if node in graph."""
+    graph.add_node('mama')
+    assert graph.has_node('mama')
 
 
-# def test_adjacent_true(graph):
-#     """Test adjacent returns true for nodes connected by edge."""
-#     graph.add_edge('beer', 'wine')
-#     assert graph.adjacent('beer', 'wine')
+def test_has_node_false(graph):
+    """Test has_node returns false if node not in graph."""
+    graph.add_node('mama')
+    assert graph.has_node('dada') is False
 
 
-# def test_adjacent_false(graph):
-#     """Test adjacent returns false for nodes not connected by edge."""
-#     graph.add_node('beer')
-#     graph.add_node('wino')
-#     assert graph.adjacent('beer', 'wino') is False
+def test_neighbors(graph):
+    """Test neighbors returns all nodes connected to a single node."""
+    graph.add_edge('car', 'bank')
+    graph.add_edge('car', 'wheels')
+    assert graph.neighbors('car') == ['bank', 'wheels']
 
 
-# def test_adjacent_reverse_is_false(graph):
-#     """Test adjacent returns false for nodes connected by edge in opposite direction."""
-#     graph.add_edge('beer', 'wine')
-#     assert graph.adjacent('wine', 'beer') is False
+def test_neighbors_error(graph):
+    """Test neighbors returns all nodes connected to a single node."""
+    graph.add_edge('car', 'bank')
+    graph.add_edge('car', 'wheels')
+    with pytest.raises(IndexError):
+        graph.neighbors('house') == ['bank', 'wheels']
+
+
+def test_adjacent_true(graph):
+    """Test adjacent returns true for nodes connected by edge."""
+    graph.add_edge('beer', 'wine')
+    assert graph.adjacent('beer', 'wine')
+
+
+def test_adjacent_false(graph):
+    """Test adjacent returns false for nodes not connected by edge."""
+    graph.add_node('beer')
+    graph.add_node('wino')
+    assert graph.adjacent('beer', 'wino') is False
+
+
+def test_adjacent_reverse_is_false(graph):
+    """Test adjacent returns false for nodes connected by edge in opposite direction."""
+    graph.add_edge('beer', 'wine')
+    with pytest.raises(KeyError):
+        graph.adjacent('wine', 'sake')
 
 
 # def test_add_duplicate_edge(graph):
