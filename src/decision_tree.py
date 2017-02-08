0d7452bf53ea15b3013f27b810f78c6568099d29
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-07 17:38:59 -0800

    added a split function to split data

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -13,12 +13,30 @@ def convert_csv(filename):
     return data
 
 
-def purity(splits, classes):
-    g_val = 0.0
-    for idx, split in enumerate(splits):
-        h_val = 0
-        for class_val in classes:
-            proportion = [row[-1] for row in split].count(class_val) / len(split)
-            h_vals[idx] += (proportion * (1.0 - proportion))
-        g_val += len(split) / len(self.size)
-    return g_val
+class Clf(object):
+    """."""
+
+    def __init__(self, iterable):
+        """Initialize clf."""
+        self._size = len(iterable)
+
+    def _purity(self, splits, classes):
+        """Determine the purity."""
+        g_val = 0.0
+        for split in splits:
+            h_val = 0
+            for class_val in classes:
+                proportion = [row[-1] for row in split].count(class_val) / len(split)
+                h_val += (proportion * (1.0 - proportion))
+            g_val += len(split) / len(self._size) * h_val
+        return g_val
+
+    def _split(self, col, boundary):
+        """Split column data based on a boundary (data coordinate)."""
+        left, right = [], []
+        for row in col:
+            if row <= boundary:
+                left.append(row)
+            else:
+                right.append(row)
+        return left, right
