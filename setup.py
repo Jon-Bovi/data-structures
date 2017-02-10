92116961d6d3a00bd6f5c913b15b6a4c83e40f6e
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-10 11:11:14 -0800

    added numpy to requires

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ setup(
     name="data-structures",
     description="Python implementations of classic data structures",
     version=0.2,
-    author=["Ford Fowler", "Casey O'Kane"],
-    author_email=["fordjfowler@gmail.com", "okanecasey@gmail.com"],
+    author=["Ford Fowler", "Casey O'Kane", "Claire Gatenby"],
+    author_email=["fordjfowler@gmail.com",
+                  "okanecasey@gmail.com",
+                  'clairejgatenby@gmail.com'],
     licencse="MIT",
     package_dir={'': 'src'},
-    py_modules=["linked_list", "stack", "dll"],
+    py_modules=["linked_list", "stack", "dll", 'decision_tree', 'qqueue'],
+    install_requires=['numpy'],
     extras_require={
-        "test": ["pytest", "pytest-cov", "tox"]
+        "test": ["pytest", "pytest-cov", "tox", 'numpy']
     }
 )
