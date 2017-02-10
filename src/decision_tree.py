f5f89aa6513bc72a16e4d9b314d42dd8769993fd
Ford Fowler <fordjfowler@gmail.com>
2017-02-09 18:38:30 -0800

    recusion working

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -16,18 +16,20 @@ def convert_csv(filename):
 class Node(object):
     """Node."""
 
-    def __init__(self, split_value, col, left=None, right=None, label=None):
+    def __init__(self, split_value, col, left=None, right=None, label=None, parent=None):
         """Initailize Node."""
         self.split_value = split_value
         self.col = col
         self.left = left
         self.right = right
         self.label = label
+        self.parent = parent
 
-        def depth(self):
-            if self.parent:
-                return 1 + self.parent.depth()
-            return 1
+    def depth(self):
+        """Return the depth of the node."""
+        if self.parent:
+            return 1 + self.parent.depth()
+        return 1
 
 
 class Clf(object):
@@ -39,6 +41,7 @@ class Clf(object):
         self.num_cols = len(iterable[0])
         self.min_leaf_size = min_leaf_size
         self.max_depth = max_depth
+        self.root = None
 
     def _h_val(self, dataset, classes):
         h_val = 0
@@ -69,9 +72,9 @@ class Clf(object):
     def _find_best_split(self, dataset):
         """Return the decision boundary where the total purity of both sides is best."""
         best_split = None
-
         best_purity = 1
-        for col_idx in range(len(dataset[0])):
+
+        for col_idx in range(len(dataset[0]) - 1):
             for row_idx, row in enumerate(dataset):
                 boundary_val = dataset[row_idx][col_idx]
                 left, right = self._split(dataset, col_idx, boundary_val)
@@ -81,21 +84,39 @@ class Clf(object):
                     best_split = (col_idx, boundary_val, left, right)
         return best_split
 
+    def _get_majority_class(self, dataset):
+        """Find and return the majority class of a dataset."""
+        classes = [row[-1] for row in dataset]
+        return max(set(classes), key=classes.count)
+
     def fit(self, dataset, classes, parent=None):
         """Construct a decision tree based on some incoming dataset; returns nothing."""
         col_idx, split_value, left, right = self._find_best_split(dataset)
-        new_node = Node(split_value, col_idx)
+        new_node = Node(split_value, col_idx, parent=parent)
+
+        if not left or not right:
+            label = self._get_majority_class(left + right)
+            return label
+
+        if new_node.depth() > self.max_depth:
+            YOU SHALL NOT PASS
 
         if self._h_val(left, classes) != 0 and len(left) > self.min_leaf_size:
             new_node.left = self.fit(left, classes, parent=new_node)
         else:
-            label = left[0][0]
-            new_node.left = Node(0, 0, label=label)
+            label = self._get_majority_class(left)
+            new_node.left = label
 
         if self._h_val(right, classes) != 0 and len(right) > self.min_leaf_size:
             new_node.right = self.fit(right, classes, parent=new_node)
         else:
-            label = right[0][0]
-            new_node.right = Node(0, 0, label=label)
+            label = self._get_majority_class(right)
+            new_node.right = label
 
-        return new_node
+        if new_node.parent:
+            return new_node
+        else:
+            self.root = new_node
+
+    def predict(self, data):
+
